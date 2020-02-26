28cf8ce630763ae4998547baa60467483732877d
Ruth Obe <ruthobey@gmail.com>
2020-02-26 16:41:23 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,6 +48,17 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  
+  
+  row_index = 0 
+  while row_index < movies_collection.count do 
+    movies_collection[row_index][:director_name] = name
+    row_index += 1 
+  end 
+  
+  p movies_collection
+  
+  
 end
 
 
@@ -63,6 +74,46 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  
+  
+  hash = {}
+  row_index = 0
+  array_of_extracted_hashes = []
+  keys_to_extract = [:studio, :worldwide_gross]
+  
+  while row_index < collection.count do
+    array_of_extracted_hashes << collection[row_index].select {|key, _| keys_to_extract.include? key}
+    
+    row_index += 1 
+  end 
+  
+  result = array_of_extracted_hashes.group_by {|h| h[:studio]}.values 
+  result.map! {|first, *rest|
+    if rest.empty?
+      first 
+    else
+      first.dup.tap {|sum|
+        rest.each {|h| sum[:worldwide_gross] += h[:worldwide_gross]}}
+    end 
+  }
+  
+  # while row_index < result.count do 
+  #   new_key = result[row_index][:studio]
+  #   new_value = result[row_index][:worldwide_gross]
+    
+  #   hash[new_key] = new_value
+    
+  #   row_index += 1
+  # end 
+  
+  new_key = result[0][:studio]
+  new_value = result[0][:worldwide_gross]
+  new_key2 = result[1][:studio]
+  new_value2 = result[1][:worldwide_gross]
+  
+  hash[new_key] = new_value
+  hash[new_key2] = new_value2
+  p hash
 end
 
 def movies_with_directors_set(source)
